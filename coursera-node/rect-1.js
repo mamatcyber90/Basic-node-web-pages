c58f328924b02aed285a968d2c6a968e02f2d252
monera23 <jiturishrikant@gmail.com>
2016-03-27 22:25:14 +0530

    message

---
+++ coursera-node/rect-1.js
@@ -0,0 +1,11 @@
+
+
+// export is an object 
+
+exports.perimeter=function(x,y){
+	return (2*(x+y));
+}
+
+exports.area=function(x,y){
+	return (x*y);
+}
---
+++ coursera-node/simmplerect-3.js
@@ -0,0 +1,26 @@
+var argv=require('yargs')
+	.argv;
+
+
+var rect=require('./rect-2');
+
+function solverect(l,b){
+
+	console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+
+	rect(l,b,function(err,rectangle){
+		if (err) {
+	    console.log(err);
+	}
+	else {
+	    console.log("The area of a rectangle of dimensions length = "
+                 + l + " and breadth = " + b + " is " + rectangle.area());
+            console.log("The perimeter of a rectangle of dimensions length = "
+                 + l + " and breadth = " + b + " is " + rectangle.perimeter());
+	}
+	});
+};
+
+
+solverect(argv.l,argv.b);
