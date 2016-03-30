08c40e9b1ccc50173a06b31aea91bc291ef3b44d
monera23 <jiturishrikant@gmail.com>
2016-03-30 10:43:09 +0530

    first commit

---
+++ basic-auth/server.js
@@ -0,0 +1,62 @@
+// simple server using express
+var express =require('express');
+var morgan=require('morgan');       // to store log details on server
+
+var hostname='localhost';
+var port=3000;
+
+var app=express();
+
+app.use(morgan('dev'));
+
+function auth(req,res,next){					// auth middleware
+	console.log(req.headers);
+
+	var authHeader= req.headers.authorization;
+
+	if(!authHeader)
+	{
+		var err= new Error('You are not authenticated!');
+
+		err.status=401;
+
+		next(err);
+		return ;
+	}
+
+	var auth=new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');    // takes auth header and parses out the use and pass 
+
+	var user=auth[0];
+	var pass=auth[1];
+
+	if(user=='admin' && pass=='password')
+	{
+		next();// authorized
+	}
+	else
+	{
+		var err= new Error('You are not authenticated');
+		err.status=401;
+		next(err);
+
+
+	}
+}
+
+app.use(auth);    // user has to pass this authentication to access the static files.
+
+app.use(express.static(__dirname +'/public'));         // to use static files 
+
+
+app.use(function(err,req,res,next){
+	res.writeHead(err.status ||500,{
+		'WWW-Authenticate': 'Basic',
+		'Content-type': 'text/plain'
+	});
+
+	res.send(err.message);
+});
+
+app.listen(port,hostname,function(){
+	console.log('Server running at http://'+hostname+':'+port+'/');
+});
